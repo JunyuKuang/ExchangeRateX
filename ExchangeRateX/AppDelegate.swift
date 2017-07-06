24cae825a33f696c01c5d1d5f803b88863d79f00
Jonny Kuang <jykuang999@gmail.com>
2017-07-06 18:51:56 +0800

    Add codable models for json

--- ExchangeRateX/AppDelegate.swift
+++ ExchangeRateX/AppDelegate.swift
@@ -10,17 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+	
+	private let url = URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")!
+	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		
 	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
-
+	
 
 }
 
---
+++ ExchangeRateX/ExchangeRateModels.swift
@@ -0,0 +1,40 @@
+//
+//  ExchangeRateModels.swift
+//  ExchangeRateX
+//
+//  Created by Jonny on 7/6/17.
+//  Copyright © 2017 Jonny. All rights reserved.
+//
+
+import Foundation
+
+// e.g.
+//{"query":{"count":1,"created":"2017-07-06T10:45:15Z","lang":"en-us","results":{"rate":{"id":"USDCNY","Name":"USD/CNY","Rate":"6.8020","Date":"7/6/2017","Time":"11:05am","Ask":"6.8041","Bid":"6.8020"}}}}
+
+struct ExchangeRateFetchResults : Codable {
+	let results: [ExchangeRateFetchResult]
+	
+	struct ExchangeRateFetchResult : Codable {
+		let rate: [ExchangeRate]
+	}
+}
+
+struct ExchangeRate : Codable {
+	let id: String
+	let name: String
+	let rate: Double
+	let date: String
+	let time: String
+	let ask: Double
+	let bid: Double
+	
+	private enum CodingKeys : String, CodingKey {
+		case id
+		case name = "Name"
+		case rate = "Rate"
+		case date = "Date"
+		case time = "Time"
+		case ask = "Ask"
+		case bid = "Bid"
+	}
+}

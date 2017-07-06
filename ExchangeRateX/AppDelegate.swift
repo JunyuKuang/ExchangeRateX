39c5a048588b8bbf9243739a9b336841294c3cfa
Jonny Kuang <jykuang999@gmail.com>
2017-07-06 19:04:38 +0800

    Add HTTP request, JSON parse, and basic UI updates based on retrieved data.

--- ExchangeRateX/AppDelegate.swift
+++ ExchangeRateX/AppDelegate.swift
@@ -13,10 +13,79 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	
 	private let url = URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")!
 	
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
+	private let statusItem: NSStatusItem = {
+		let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+		statusItem.button?.title = "ExchangeRateX"
 		
+		let menu = NSMenu()
+		let refreshItem = NSMenuItem(title: "Refresh", action: #selector(tapRefreshMenuItem), keyEquivalent: "R")
+		let exitItem = NSMenuItem(title: "Exit", action: #selector(tapExitMenuItem), keyEquivalent: "Q")
+		menu.addItem(refreshItem)
+		menu.addItem(exitItem)
+		statusItem.menu = menu
+		
+		return statusItem
+	}()
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		refresh()
+		Timer.scheduledTimer(timeInterval: 10 * 60, target: self, selector: #selector(refresh), userInfo: nil, repeats: true)
 	}
 	
+	@objc private func refresh() {
+		
+		URLSession.shared.dataTask(with: url) { data, _, error in
+			if let data = data {
+				do {
+					let query = try JSONDecoder().decode(ExchangeRateQuery.self, from: data)
+					let exchangeRate = query.query.results.rate
+					DispatchQueue.main.async {
+						self.updateUI(with: exchangeRate)
+					}
+				} catch {
+					print(error)
+					DispatchQueue.main.async {
+						let alert = NSAlert(error: error)
+						alert.runModal()
+					}
+				}
+			} else if let error = error {
+				print(error)
+				DispatchQueue.main.async {
+					let alert = NSAlert(error: error)
+					alert.runModal()
+				}
+			}
+			}.resume()
+	}
+	
+	private func updateUI(with rate: ExchangeRate) {
+		guard let rateDoubleValue = Double(rate.rate) else { return }
+		statusItem.button?.title = String(format: " $1 = ¥%.3f ", rateDoubleValue)
+		
+//		let menu = NSMenu()
+//		let items = [NSMenuItem(title: "Updated at " + DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .medium), action: nil, keyEquivalent: "")]
+//			+ [NSMenuItem.separator()]
+//			+ forecastMenuItems
+//			+ [NSMenuItem.separator()]
+//			+ aqiMenuItems
+//			+ [NSMenuItem.separator()]
+//			+ [NSMenuItem(title: "Refresh", action: #selector(tapRefreshMenuItem), keyEquivalent: "R"),
+//			   NSMenuItem(title: "Exit", action: #selector(tapExitMenuItem), keyEquivalent: "Q")]
+//
+//		items.forEach(menu.addItem)
+//		statusItem.menu = menu
+	}
+	
+	@objc private func tapGeneralItem(_ sender: NSMenuItem) {}
+	
+	@objc private func tapRefreshMenuItem(_ sender: NSMenuItem) {
+		refresh()
+	}
+	
+	@objc private func tapExitMenuItem(_ sender: NSMenuItem) {
+		NSApp.terminate(self)
+	}
 
 }
 
--- ExchangeRateX/ExchangeRateModels.swift
+++ ExchangeRateX/ExchangeRateModels.swift
@@ -11,22 +11,26 @@ import Foundation
 // e.g.
 //{"query":{"count":1,"created":"2017-07-06T10:45:15Z","lang":"en-us","results":{"rate":{"id":"USDCNY","Name":"USD/CNY","Rate":"6.8020","Date":"7/6/2017","Time":"11:05am","Ask":"6.8041","Bid":"6.8020"}}}}
 
-struct ExchangeRateFetchResults : Codable {
-	let results: [ExchangeRateFetchResult]
+struct ExchangeRateQuery : Codable {
+	let query: ExchangeRateFetchResults
 	
-	struct ExchangeRateFetchResult : Codable {
-		let rate: [ExchangeRate]
+	struct ExchangeRateFetchResults : Codable {
+		let results: ExchangeRateFetchResult
+		
+		struct ExchangeRateFetchResult : Codable {
+			let rate: ExchangeRate
+		}
 	}
 }
 
 struct ExchangeRate : Codable {
 	let id: String
 	let name: String
-	let rate: Double
+	let rate: String
 	let date: String
 	let time: String
-	let ask: Double
-	let bid: Double
+	let ask: String
+	let bid: String
 	
 	private enum CodingKeys : String, CodingKey {
 		case id
